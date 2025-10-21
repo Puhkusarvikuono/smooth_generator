91fd0f7d711d7273ff37f21cd5460a65e2136f92
Puhkusarvikuono <jyrki.vassinen@proton.me>
2025-10-21 18:04:26 +0300

    src and basic stuff

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode 
+
+def main():
+    new_object = TextNode("some text", "plain", "https://dummysite.net")
+    print(new_object)
+
+if __name__ == "__main__":
+    main()
